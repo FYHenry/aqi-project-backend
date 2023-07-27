15c9fc21cd6e32fa3f29baa8b3523b30de216b3c
NicolasSoulay <soulaynicolas@gmail.com>
2023-07-27 12:29:05 +0200

    init

---
+++ src/main/java/fr/diginamic/aqiprojectbackend/AqiProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.aqiprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqiProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqiProjectBackendApplication.class, args);
+	}
+
+}
