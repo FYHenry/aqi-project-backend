679995dee0595643e8c73ee21510a271ab065b40
NicolasSoulay <soulaynicolas@gmail.com>
2023-07-27 15:23:52 +0200

    init entity

--- src/main/java/fr/diginamic/aqiprojectbackend/entity/account/Address.java
+++ src/main/java/fr/diginamic/aqiprojectbackend/entity/account/Address.java
@@ -1,5 +1,53 @@
 package fr.diginamic.aqiprojectbackend.entity.account;
 
-@entity
+import fr.diginamic.aqiprojectbackend.entity.map.City;
+
 public class Address {
+    private Integer id;
+    private String addressLine1;
+    private String addressLine2;
+
+    private City city;
+
+    public Address(){
+    }
+
+    public Address(Integer id, String addressLine1, String addressLine2, City city) {
+        this.id = id;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
 }
--- src/main/java/fr/diginamic/aqiprojectbackend/entity/account/UserAccount.java
+++ src/main/java/fr/diginamic/aqiprojectbackend/entity/account/UserAccount.java
@@ -1,4 +1,12 @@
 package fr.diginamic.aqiprojectbackend.entity.account;
 
 public class UserAccount {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private UserStatus userStatus;
+    private Address address;
 }
