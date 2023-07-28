7632589ec0f7793b225b3adb102ad7c490e426b8
NicolasSoulay <soulaynicolas@gmail.com>
2023-07-28 15:34:57 +0200

    requete get ok

--- src/main/java/fr/diginamic/aqiprojectbackend/controller/UserController.java
+++ src/main/java/fr/diginamic/aqiprojectbackend/controller/UserController.java
@@ -1,17 +1,7 @@
 package fr.diginamic.aqiprojectbackend.controller;
 
 import fr.diginamic.aqiprojectbackend.entity.account.UserAccount;
-import fr.diginamic.aqiprojectbackend.repository.account.AddressRepository;
-//import fr.diginamic.aqiprojectbackend.repository.account.CredentialRepository;
-import fr.diginamic.aqiprojectbackend.repository.account.UserAccountRepository;
-import fr.diginamic.aqiprojectbackend.repository.account.UserStatusRepository;
-import fr.diginamic.aqiprojectbackend.repository.forum.MessageRepository;
-import fr.diginamic.aqiprojectbackend.repository.forum.ReactionRepository;
-import fr.diginamic.aqiprojectbackend.repository.forum.ThreadRepository;
-import fr.diginamic.aqiprojectbackend.repository.forum.TopicRepository;
-import fr.diginamic.aqiprojectbackend.repository.map.BookmarkRepository;
 import fr.diginamic.aqiprojectbackend.service.UserService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
