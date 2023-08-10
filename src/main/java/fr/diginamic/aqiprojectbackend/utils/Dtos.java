5f723b7aa6930b16faf80d2b6610f1967639123d
FYHenry <frederic_yann.henry@protonmail.com>
2023-08-10 22:59:17 +0200

    Utils : code factoring

--- src/main/java/fr/diginamic/aqiprojectbackend/utils/Dtos.java
+++ src/main/java/fr/diginamic/aqiprojectbackend/utils/Dtos.java
@@ -2,6 +2,8 @@ package fr.diginamic.aqiprojectbackend.utils;
 
 import fr.diginamic.aqiprojectbackend.dto.HttpStatusDtoOut;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 /** Data Transfer Objects utility */
@@ -20,6 +22,24 @@ final public class Dtos {
                 path);
     }
 
+    /**
+     * HTTP status response builder
+     * @param httpStatus HTTP status
+     * @param path Request path
+     * @return HTTP status response
+     */
+    public static ResponseEntity<HttpStatusDtoOut>
+    buildHttpStatusResponse(HttpStatus httpStatus, String path){
+        final HttpStatusDtoOut output =
+                new HttpStatusDtoOut(LocalDateTime.now(),
+                        httpStatus.value(),
+                        httpStatus.getReasonPhrase(),
+                        path);
+        return ResponseEntity
+                .status(httpStatus)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(output);
+    }
     /**
      * Constructor.
      */
