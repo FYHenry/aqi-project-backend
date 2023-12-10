5c4cedaf7fe2e23e4b294a050de24a0caa78fb19
FYHenry <frederic_yann.henry@protonmail.com>
2023-12-10 11:47:01 +0100

    Externalized AQI version

--- src/main/java/fr/diginamic/aqiprojectbackend/AqiProjectBackendApplication.java
+++ src/main/java/fr/diginamic/aqiprojectbackend/AqiProjectBackendApplication.java
@@ -2,6 +2,8 @@ package fr.diginamic.aqiprojectbackend;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -19,10 +21,13 @@ import static java.util.ResourceBundle.getBundle;
         )
 )
 public class AqiProjectBackendApplication {
+    private static final Logger logger =
+            LoggerFactory.getLogger(AqiProjectBackendApplication.class);
     public static final String aqiVersion =
-            getBundle("application").getString("aqi.version");
+            getBundle("application").getString("project.version");
     /** Main method */
     public static void main(String[] args) {
         SpringApplication.run(AqiProjectBackendApplication.class, args);
+        logger.info("AQI backend version : {}", aqiVersion);
     }
 }
